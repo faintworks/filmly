12516299d8460389ff02f93fbba32293e403f7e9
faintworks <iiro.laitinen04@gmail.com>
2025-09-21 22:43:49 +0300

    Add review post feature

--- app.py
+++ app.py
@@ -8,10 +8,27 @@ import db
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    movie = request.form["movie"]
+    review = request.form["review"]
+    score = request.form["score"]
+    user_id = session["user_id"]
+    
+    sql = """INSERT INTO items (movie, review, score, user_id) 
+             VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [movie, review, score, user_id])
+
+    return redirect("/")
+               
 
 @app.route("/register")
 def register():
@@ -40,14 +57,16 @@ def login():
          return render_template("login.html")
     
     if request.method == "POST":
-        
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -55,5 +74,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    movie TEXT,
+    review TEXT,
+    score INTEGER,
+    user_id INTEGER REFERENCES users
+);
